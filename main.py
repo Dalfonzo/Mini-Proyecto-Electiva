4c31160163e18b778ee44a7d5dcbcc352cdd1a14
dap <dalfonzo18@gmail.com>
2020-11-10 17:11:46 -0400

    avances

--- main.py
+++ main.py
@@ -58,30 +58,30 @@ ingredients = [
 def size_menu():
     while True:
         ids = []
-        print("Tamaños:", end=' ')
+        print("Tamaños:", end=" ")
         for size in sizes:
             ids.append(size["id"])
-            print(f'{size["size"]} ( {size["id"]} )', end=' ')
-        print(":", end=' ')
-        option = input('')
+            print(f"{size['size']} ( {size['id']} )", end=" ")
+        print(":", end=" ")
+        option = input("")
         if option.lower() in ids:
             return option
         print("=> Debe seleccionar el tamaño correcto!!")
 
 
 def ingredients_menu():
-    extra_ing = set()
+    extra_ing, ids = [], []
+    print("Ingredientes:")
+    for ing in ingredients:
+        ids.append(ing["id"])
+        print(f"{ing['name']} ( {ing['id']} )")
+    print()
     while True:
-        ids = []
-        print("Ingredientes:")
-        for ing in ingredients:
-            ids.append(ing["id"])
-            print(f'{ing["name"]} ( {ing["id"]} )')
-        option = input('\nIndique ingrediente (enter para terminar): ')
+        option = input("Indique ingrediente (enter para terminar): ")
         if option.lower() in ids:
-            extra_ing.add(option.lower())
+            extra_ing.append(option.lower())
         elif option == "":
-            return list(extra_ing)
+            return extra_ing
         else:
             print("=> Debe seleccionar un ingrediente correcto!!\n")
 
@@ -89,32 +89,44 @@ def ingredients_menu():
 def generate_bill(pizza_size, extra_ing):
     size_list = list(filter(lambda el: el["id"] in pizza_size, sizes))
     subtotal = size_list[0]["price"]
+    aux_list = []
     print(f"Usted seleccionó una pizza {size_list[0]['size']}", end=" ")
     if not extra_ing:
         print("Margarita\n")
     else:
         ing_list = list(filter(lambda el: el["id"] in extra_ing, ingredients))
-        print("con: ", end=" ")
         for ing in ing_list:
             subtotal += ing["price"]
-            print(ing["name"], end=" ")
-        print("\n")  #Cambiar
+            aux_list.append(ing["name"])
+        print(f"con: {', '.join(aux_list)}\n")
     print(f"Subtotal a pagar por una pizza {size_list[0]['size']}: {subtotal}")
     return subtotal
 
 
 def main_menu():
+    order_total = []
     hr_line = "*" * 30
     print(hr_line)
     print(f"*{'PIZZERIA UCAB'.center(28)}*")
     print(hr_line)
-    print("\nOpciones")
-    size, extra_ing = size_menu(), ingredients_menu()
-    generate_bill(size, extra_ing)
-    print(hr_line)
-
-    print(extra_ing)
-    print(size)
+    while True:
+        print(f"Pizza número {len(order_total) + 1}")
+        print("\nOpciones")
+        size, extra_ing = size_menu(), ingredients_menu()
+        order_total.append(generate_bill(size, extra_ing))
+        print(hr_line)
+        while True:
+            res = input("¿Desea continuar [s/n]?: ")
+            print(hr_line)
+            if res == "n" or res == "s":
+                break
+            print("=> Debe seleccionar una opción correcta!!")
+        if res.lower() == "n":
+            print(
+                f"El pedido tiene un total de {len(order_total)} pizza(s) por un monto de {sum(order_total)}\n"
+            )
+            print("Gracias por su compra, regrese pronto")
+            break
 
 
 main_menu()
